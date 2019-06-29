b50bf91c3dd5faec0ace9c2e2bc333ab84bc0c2d
tkhang <haphongpk12@gmail.com>
2019-06-29 13:41:05 +0700

    Done WP 18

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(document).ready(
-    
-)
+$(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+    $(".js-blur").css({
+        '-webkit-transform': 'translateY(-' + scroll + 'px)',
+        'transform': 'translateY(-' + scroll + 'px)',
+    });
+});
+
+$(function () {
+    $("main").clone().prependTo($("header")).addClass("js-blur");
+    $(".js-blur").css({
+        '-webkit-filter': 'blur(.2em)',
+        'filter': 'blur(.2em)',
+        'background': '#fff',
+    });
+});
